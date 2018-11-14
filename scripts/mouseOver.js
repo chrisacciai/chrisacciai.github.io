484c04fb5f4b4d4f9d0360caa092684eaf305772
Christopher <chris@acciai.us>
2018-11-14 17:28:23 -0500

    test

--- scripts/mouseOver.js
+++ scripts/mouseOver.js
@@ -1,4 +1,10 @@
-  <script src="js/data.js"></script>
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})  <script src="js/data.js"></script>
   <script>
    var container = document.getElementById('anim_container');
     // Set up our animation 
