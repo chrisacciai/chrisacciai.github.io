c794e6790188f00be7390c4cff30117d676a903c
Christopher <chris@acciai.us>
2018-12-03 23:30:59 -0500

    test

--- underline.js
+++ underline.js
@@ -12,14 +12,15 @@ var params2 = {
 var anim2;
 anim2 = lottie.loadAnimation(params2);
 
-anim.addEventListener('DOMLoaded', function(e) {
+anim2.addEventListener('DOMLoaded', function(e) {
 
   var elem = document.getElementById('about');
 
   elem.addEventListener('mouseover', mouseElem);
 
   function mouseElem() {
-    anim2.goToAndStop(1, true);
+	anim2.setDirection(1);
+	anim2.play();
   }
 
 });
