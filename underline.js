0f47cffead6a14986afbca84965bf39dbcfd1d4f
Christopher <chris@acciai.us>
2018-12-03 23:29:16 -0500

    test

--- underline.js
+++ underline.js
@@ -1,4 +1,4 @@
-animContainer2 = document.getElementById('lottie')
+animContainer2 = document.getElementById('lottie');
  				
 var params2 = {
 	container: animContainer2,
@@ -8,18 +8,18 @@ var params2 = {
 	autoloadSegments: true,
 	path: 'underline.json'
 };
- 
+
 var anim2;
 anim2 = lottie.loadAnimation(params2);
-				
-animContainer2.addEventListener("mouseover", forwardAnimation);
-animContainer2.addEventListener("mouseout", reverseAnimation);
-			
-function forwardAnimation() {
-	anim2.setDirection(1);
-	anim2.play();
-}
-function reverseAnimation() {
-	anim2.setDirection(-1);
-	anim2.play();
-}
+
+anim.addEventListener('DOMLoaded', function(e) {
+
+  var elem = document.getElementById('about');
+
+  elem.addEventListener('mouseover', mouseElem);
+
+  function mouseElem() {
+    anim2.goToAndStop(1, true);
+  }
+
+});
